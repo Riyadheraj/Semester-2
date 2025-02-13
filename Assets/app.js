6465f7baa3bba4bd74457aaceb496657eeffbb8e
Riyaaptech@ <riyadherajdevria@gmail.com>
2025-02-13 13:52:46 +0500

    recently files upload

---
+++ Assets/app.js
@@ -0,0 +1,58 @@
+// var name ="ali";
+// var name = "zain";
+
+// document.write(name);
+
+
+// var name ="ali";
+// var name = "zain";
+
+// document.write(name);
+
+// var name = "ahmed";
+
+// let name = "ali";
+// name = "hassan";
+
+// function myapp(num1, num2){
+//    document.write(10,20)
+
+// }
+
+// myapp
+
+// let num1 = +prompt("Enter First Value");
+// let num2 = +prompt("Enter Second Value");
+
+
+
+// function myapp(num1, num2){
+//    document.write(num1 + num2);
+// }
+
+// myapp(num1, num2);
+
+// function app(name){
+//    return name
+// }
+//  let result = app("ali <br>");
+//  document.write(result)
+
+
+// function myapp(num1, num2, num3){
+//    return num1 + num2 - num3
+// }
+
+// document.write(myapp(20,40,50))
+
+// let myapp = (name) => document.write(name)
+
+// myapp("ali")
+
+let value1 = +prompt("Enter First Value")
+let value2 = +prompt("Enter Second Value")
+
+let myapp = (value1, value2) => {return value1 - value2}
+
+document.write(myapp(value1, value2));
+
