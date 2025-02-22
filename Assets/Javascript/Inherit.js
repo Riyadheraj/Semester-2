35c9d73880fc1747bd2af92a4456a9911e61bc19
Riyaaptech@ <riyadherajdevria@gmail.com>
2025-02-21 22:04:57 -0800

    new

---
+++ Assets/Javascript/Inherit.js
@@ -0,0 +1,30 @@
+class A {
+
+    constructor(myname){
+        this.name = myname
+        document.write("This is constructor method from Parent Class <br>")
+    }
+  
+      
+    message(myemail){
+        this.name = myemail
+        document.write(`Hello, from parent class <br> ${this.name} ${this.email} <br>`)
+    }
+}
+
+class B extends A{
+
+    constructor(mypass){
+        super("ali")
+        this.pass = mypass
+        document.write(`This is constructor method from Child Class <br> ${this.pass} <br>`)
+    }
+
+    display(){
+        document.write(`Hello, from child class <br> ${this.name}`)
+    }
+}
+
+let obj = new B(12345);
+obj.message("ali@gmail.com");
+obj.display()
